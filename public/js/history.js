bc9b360f658fee51f424287079312baf1f0b8723
Koushik Naik <koushiknaik006@gmail.com>
2025-12-26 10:54:00 +0530

    Delete public directory

--- public/js/history.js
+++
@@ -1,9 +0,0 @@
-
-fetch('/api/history')
-.then(r=>r.json())
-.then(d=>{
- new Chart(document.getElementById('historyChart'),{
-  type:'bar',
-  data:{labels:d.labels,datasets:[{label:'Steps',data:d.steps}]}
- });
-});
